7333a5ae3047840cb61d27d4f4a5b6adb8bafd80
moxuse <moxuse@gmail.com>
2019-09-03 19:51:55 +0900

    gloabl scene object

---
+++ src/Graphics/Yodaka/Port.purs
@@ -0,0 +1,19 @@
+module Graphics.Yodaka.Port where
+
+import Effect
+import Graphics.Three.Scene as Scene
+
+import Data.Foreign.EasyFFI (unsafeForeignFunction)
+
+
+-- data TargetRenderer = TargetRenderer
+--  {target:: WebGLRenderTarget, scene :: Scene }
+  
+type Port =
+  { scene :: Scene.Scene
+  -- , renderers :: Array TargetRenderer
+  }
+
+
+gloabalPort :: Effect Port
+gloabalPort = unsafeForeignFunction [""] "window.port"
